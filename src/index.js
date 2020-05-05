969ef68f325419791aaa1f35574676ec733e9472
Hamza Mahmud <hamza.mahmud@digitalbridge.com>
2020-05-05 14:48:07 +0100

    replace debug with console.log

--- src/index.js
+++ src/index.js
@@ -3,57 +3,57 @@ import * as prodLytics from "./analytics/prod";
 import * as devLytics from "./analytics/dev";
 
 function isLocal(host) {
-  return location.hostname === host;
+    return location.hostname === host;
 }
 
 function isDev() {
-  return process.env.NODE_ENV !== "production";
+    return process.env.NODE_ENV !== "production";
 }
 
-export default (code, Router, { localhost = "localhost" } = {}) => Page => {
-  class WithAnalytics extends Component {
-    state = {
-      analytics: undefined
-    };
+export default (code, Router, { localhost = "localhost" } = {}) => (Page) => {
+    class WithAnalytics extends Component {
+        state = {
+            analytics: undefined,
+        };
 
-    componentDidMount() {
-      // check if it should track
-      const shouldNotTrack = isLocal(localhost) || isDev();
-      // check if it should use production or dev analytics
-      const analytics = shouldNotTrack ? devLytics : prodLytics;
+        componentDidMount() {
+            // check if it should track
+            const shouldNotTrack = isLocal(localhost) || isDev();
+            // check if it should use production or dev analytics
+            const analytics = shouldNotTrack ? devLytics : prodLytics;
 
-      // init analytics
-      analytics.init(code);
-      // log page
-      analytics.pageview();
+            // init analytics
+            analytics.init(code);
+            // log page
+            analytics.pageview();
 
-      // save possible previously defined callback
-      const previousCallback = Router.onRouteChangeComplete;
-      Router.onRouteChangeComplete = () => {
-        // call previously defined callback if is a function
-        if (typeof previousCallback === "function") {
-          previousCallback();
+            // save possible previously defined callback
+            const previousCallback = Router.onRouteChangeComplete;
+            Router.onRouteChangeComplete = () => {
+                // call previously defined callback if is a function
+                if (typeof previousCallback === "function") {
+                    previousCallback();
+                }
+                // log page
+                analytics.pageview();
+            };
+
+            this.setState({
+                analytics,
+            });
         }
-        // log page
-        analytics.pageview();
-      };
 
-      this.setState({
-        analytics
-      });
+        render() {
+            return createElement(Page, {
+                ...this.props,
+                analytics: this.state.analytics,
+            });
+        }
     }
 
-    render() {
-      return createElement(Page, {
-        ...this.props,
-        analytics: this.state.analytics
-      });
+    if (Page.getInitialProps) {
+        WithAnalytics.getInitialProps = Page.getInitialProps;
     }
-  }
 
-  if (Page.getInitialProps) {
-    WithAnalytics.getInitialProps = Page.getInitialProps;
-  }
-
-  return WithAnalytics;
+    return WithAnalytics;
 };
